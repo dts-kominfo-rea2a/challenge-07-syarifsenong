7ae93955f32edb9db0efc5605f8c4e5457fbae15
Syarif Nurhidayah <syarif.nurhid@gmail.com>
2022-06-12 23:03:58 +0700

    fix 07

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let a = await promiseTheaterIXX();
+  let b = await promiseTheaterVGC();
+  let gabung = a.concat(b);
+  let total = 0;
+  gabung.map((a) => a.hasil === emosi && (total += 1));
+  return total;
+};
 
 module.exports = {
   promiseOutput,
